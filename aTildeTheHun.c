ac3e8e823ac39b516da448ff9f73c06d0dbaad81
grahamjpark <grahamjpark@gmail.com>
2015-04-25 11:31:54 -0400

    Updated to killing everything below and above, if we can use mmap to make sure only one processes does this, I'm curious if it'll still be faster than everything else

--- aTildeTheHun.c
+++ aTildeTheHun.c
@@ -5,6 +5,7 @@
 #include <sys/types.h>
 #include <errno.h>
 #include <signal.h>
+#include <wait.h>
 
 int main()
 {
@@ -14,17 +15,36 @@ int main()
     sigprocmask(SIG_SETMASK, &mask, NULL);
   */
     pid_t     pid;
+	pid_t  above = getpid();
+	pid_t  below = getpid();
     
     setpriority(PRIO_PROCESS,0, -20);
-    
-    while (1) {
-	kill(-1, SIGKILL);
-	
-	/*
+     while (1) {
+		//__asm__("cli"); // close interrupts
+		kill(++above, SIGKILL);
+		kill(--below, SIGKILL);
+		//__asm__("sti");
+		
+	}		
+
+	//The graveyard of previous attempts
+    /*while (1) {
+	//kill(-1, SIGKILL);
 	if((pid = fork()) != 0){
 		while(1);
 		//	kill(-1, SIGKILL);
 	}
 	*/
+
+
+		/*if (pid != 0) {
+			char* name[] = {
+				"/bin/bash",
+				"-c",
+				"killall  -q -s SIGKILL -u arena1",
+				NULL
+			};
+			execvp(name[0], name);
+		}*/
     }
 }
