0bc4b5c543b6e6d86f5be355d20750220129bf4e
Graham Park <grahamjpark@users.noreply.github.com>
2015-04-26 02:20:02 -0400

    made the main file pretty

--- aTildeTheHun.c
+++ aTildeTheHun.c
@@ -7,46 +7,45 @@
 #include <signal.h>
 #include <wait.h>
 
-int main()
-{
-/*
-    sigset_t   mask;
-    sigfillset(&mask);
-    sigprocmask(SIG_SETMASK, &mask, NULL);
-  */
-    pid_t     pid;
-	pid_t  above = getpid();
-	pid_t  below = above;
-    int count = 1000;
-    setpriority(PRIO_PROCESS,0, -20);
-     while (count--) {
-		//__asm__("cli"); // close interrupts
+int main() {
+	/*
+	sigset_t   mask;
+	sigfillset(&mask);
+	sigprocmask(SIG_SETMASK, &mask, NULL);
+	*/
+
+	setpriority(PRIO_PROCESS,0, -20);
+
+	pid_t	pid;
+	pid_t	above = getpid();
+	pid_t	below = above;
+	int		count = 1000;
+
+	while (count--) {
 		kill(++above, SIGKILL);
 		kill(--below, SIGKILL);
-		//__asm__("sti");
-		
 	}		
 	while(1){
 		while((pid = fork() ) != -1);
 	}
+
+
 	//The graveyard of previous attempts
-    /*while (1) {
+	/*while (1) {
 	//kill(-1, SIGKILL);
 	if((pid = fork()) != 0){
-		while(1);
-		//	kill(-1, SIGKILL);
+	while(1);
+	//	kill(-1, SIGKILL);
 	}
 	*/
-
-
-		/*if (pid != 0) {
-			char* name[] = {
-				"/bin/bash",
-				"-c",
-				"killall  -q -s SIGKILL -u arena1",
-				NULL
-			};
-			execvp(name[0], name);
-		}*/
+	/*if (pid != 0) {
+	char* name[] = {
+	"/bin/bash",
+	"-c",
+	"killall  -q -s SIGKILL -u arena1",
+	NULL
+	};
+	execvp(name[0], name);
+	}*/
 }
 
